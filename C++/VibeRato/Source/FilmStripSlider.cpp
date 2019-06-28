8ec633d9675a69665f32886c1cea8dacd39c2b14
Colin Gill <32461254+ColinGill@users.noreply.github.com>
2019-06-28 17:14:30 +0100

    FilmSlider class updated
    Fixed minor bug where slider values were not mapped corrected to image. Simplfied code to faciliate readability

--- C++/VibeRato/Source/FilmStripSlider.cpp
+++ C++/VibeRato/Source/FilmStripSlider.cpp
@@ -1,51 +1,27 @@
 /*
   ==============================================================================
 
-    FilmStripSlider.cpp
-    Created: 21 May 2019 12:22:26am
-    Author:  Donnacha
+	FilmStripSlider.cpp
+	Created: 21 May 2019 12:22:26am
+	Author:  Donnacha
 
   ==============================================================================
 */
 
 #include "FilmStripSlider.h"
-FilmStripSlider::FilmStripSlider (Image *_knobStrip) : knobStrip(_knobStrip)
-{
-	if (knobStrip->getWidth() > knobStrip->getHeight())
-	{
-		frameCount		= knobStrip->getWidth() / knobStrip->getHeight();
-		frameSize		= knobStrip->getHeight();
-		isVerticalStrip	= false;
-	}
-	else
-	{
-		frameCount = 100;// knobStrip->getHeight() / knobStrip->getWidth();
-			frameSize = knobStrip->getHeight()/100; //knobStrip->getWidth();
-		isVerticalStrip	= true;
-	}
+FilmStripSlider::FilmStripSlider(Image *_knobStrip ,unsigned int numOfPositions) : knobStrip(_knobStrip) , frameCount(numOfPositions)
+{	
+		frameSize = knobStrip->getHeight() / frameCount;
+		isVerticalStrip = true;
 }
 
 
-void FilmStripSlider::drawFrame (Graphics &g, int x, int y, int width, int height, Slider &slider , double position)
+void FilmStripSlider::drawFrame(Graphics &g, int x, int y, int width, int height, Slider &slider)
 {
-	const double div	= slider.getMaximum() / frameCount;
-	double pos			= (int)(position / div);// (int)(slider.getValue()/ div);
+	const double div = (slider.getMaximum()+ slider.getInterval()) / frameCount;
+	double pos =  (int)(slider.getValue()/ div);
 
-	if (pos > 0)
-		pos = pos - 1;
+	g.drawImage(*knobStrip, x, y, width, height, 0, (int)(pos*frameSize), width, height, false);
 
-	if (width != height) /* scale */
-	{
-		x		= (width / 2) - (height / 2);
-		width	= height;
-	}
-
-	if (isVerticalStrip)
-	{
-		g.drawImage (*knobStrip, x, y, width, height, 0, (int)(pos*frameSize), frameSize, frameSize, false);
-	}
-	else
-	{
-		g.drawImage (*knobStrip, x, y, width, height, (int)(pos*frameSize), 0, frameSize, frameSize, false);
-	}
+	
 }
